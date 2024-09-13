9b7628c6a688487d728132364e2dd45c1aca03e0
Saud Ahmed <ahmedsaud00785@gmail.com>
2024-09-13 17:25:58 +0000

    add file

---
+++ 01basics/)4_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// the reason is that an equality check == and comparisons > , <>= , <= work differently   comaparisons convert null to a number , treating it as 0   that's why (3) NULL >= 0  IS true and (1) null > 0 is false 
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// // === 
+
+// console.log("2" === 2);
