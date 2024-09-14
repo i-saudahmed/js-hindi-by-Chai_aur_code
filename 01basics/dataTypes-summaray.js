831d19c4bdbf6aab3626fac66aa5b1f41e3f692a
Saud Ahmed <ahmedsaud00785@gmail.com>
2024-09-14 09:49:24 +0000

    update

--- 01basics/dataTypes-summaray.js
+++ 01basics/dataTypes-summaray.js
@@ -6,21 +6,20 @@ JavaScript is dynamically typed, which means that the interpreter assigns a vari
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+const score = 100;
+const scoreValue = 100.3;
 
-const isLoggedIn = false
-const outsideTemp = null
+const isLoggedIn = false;
+const outsideTemp = null;
 let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+const id = Symbol("123");
+const anotherId = Symbol("123");
 
 // console.log(id === anotherId);
 
-const bigNumber = 3456543576654356754n
+const bigNumber = 3456543576654356754n;
 // console.log(typeof bigNumber)
-
 
 // Reference (Non primitive)
 
@@ -28,16 +27,33 @@ const bigNumber = 3456543576654356754n
 
 const heros = ["shaktiman", "naagraj", "doga"];
 let myObj = {
-    name: "hitesh",
-    age: 22,
-}
+  name: "hitesh",
+  age: 22,
+};
 
-const myFunction = function(){
-    console.log("Hello world");
-}
-// function return type of function but is actually a object function 
-// and also if we store it in any variable it remains function 
+const myFunction = function () {
+  console.log("Hello world");
+};
+// function return type of function but is actually a object function
+// and also if we store it in any variable it remains function
 
 // console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// memory management
+// stack and heap
+
+// stack (permitive), heap(non-permitive)
+
+// in stack we get like variable copy like boolean , number etc
+// in heap like for object we get refrence of original value
+
+let myYoutubeName = "saudahmed";
+
+let anotherName = myYoutubeName;
+anotherName = "chaiaurcode";
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
