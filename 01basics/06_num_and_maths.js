43d35cde5de285ba6658977d362ce47f209f8dfa
Saud Ahmed <ahmedsaud00785@gmail.com>
2024-09-16 17:23:26 +0000

    update

--- 01basics/06_num_and_maths.js
+++ 01basics/06_num_and_maths.js
@@ -28,3 +28,6 @@ const hundreds = 1000000;
 // console.log(Math.random());
 // console.log((Math.random()*10) + 1);
 // console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
