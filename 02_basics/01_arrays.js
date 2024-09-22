13e7ee0d8b40d49b728c28096f0c613eaf169958
Saud Ahmed <ahmedsaud00785@gmail.com>
2024-09-22 10:18:55 +0000

    update file

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,7 +1,22 @@
 // array
 
-const myArr = [0, 1, 2, 3, 4, 5]
-const myHeors = ["shaktiman", "naagraj"]
+const myArr = [ 1, 2, 3, 4, 5 , 6];
+const myHeors = ["shaktiman", "naagraj"];
 
-const myArr2 = new Array(1, 2, 3, 4)
+const myArr2 = new Array(1, 2, 3, 4);
 // console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(34); // not optimized we are forwarding the index of every value
+// myArr.shift();
+// myArr.shift();
+
+console.log(myArr);
+
+console.log(myArr.includes(4));
+console.log(myArr.indexOf(5));
