9b49e1e928d95944d8f3560575b95c5c764aebf2
Saud Ahmed <ahmedsaud00785@gmail.com>
2024-09-22 08:33:13 +0000

    update

--- 01basics/06_num_and_maths.js
+++ 01basics/06_num_and_maths.js
@@ -11,7 +11,7 @@ const balance = new Number(100);
 const otherNumber = 123.8966;
  
 // console.log(otherNumber.toPrecision(4));
-
+ 
 const hundreds = 1000000;
 // console.log(hundreds.toLocaleString('en-IN'));
 
@@ -26,6 +26,7 @@ const hundreds = 1000000;
 // console.log(Math.max(4, 3, 6, 8)); 
   
 // console.log(Math.random());
+// console.log(Math.random()*10);
 // console.log((Math.random()*10) + 1);
 // console.log(Math.floor(Math.random()*10) + 1);  
 
