189d20728a0e0f1f2947cad32d9c7aeaaa4c256c
Saud Ahmed <ahmedsaud00785@gmail.com>
2024-09-25 17:34:23 +0000

    updaate

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -37,6 +37,6 @@ console.log(myn1); // 1, 2
 console.log("B ", myArr);
    
 const myn2 = myArr.splice(1, 3) 
-console.log(myn2); // 1,2,3
+console.log(myn2); // 1,2,3 
  
 console.log("C ", myArr);     
