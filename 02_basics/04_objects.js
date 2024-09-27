01be09ae1479ba9975a9a2808c9fd1fb773ce6bc
Saud Ahmed <ahmedsaud00785@gmail.com>
2024-09-27 19:13:03 +0000

    update

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -57,7 +57,7 @@ const users = [
 // console.log(tinderUser);
 
 // console.log(Object.keys(tinderUser));
-// console.log(Object.values(tinderUser)); 
+// console.log(Object.values(tinderUser));  
 // console.log(Object.entries(tinderUser)); 
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
