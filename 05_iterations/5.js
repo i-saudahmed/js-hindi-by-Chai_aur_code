402877c960469208ee5cf7a171e38d37e7ddc57d
Saud Ahmed <ahmedsaud00785@gmail.com>
2024-10-02 16:28:06 +0000

    add file

---
+++ 05_iterations/5.js
@@ -0,0 +1,38 @@
+const coding = ["js", "ruby", "java", "python", "cpp"];
+
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(curElem) {
+//   console.log(curElem);
+// }
+
+// coding.forEach(printMe); // gives values without array not array
+
+coding.forEach((curElem, curIndex, arrList) => {
+  console.log(curElem, curIndex, arrList);
+});
+
+const myCoding = [
+  {
+    languageName: "javascript",
+    languageFileName: "js",
+  },
+  {
+    languageName: "java",
+    languageFileName: "java",
+  },
+  {
+    languageName: "python",
+    languageFileName: "py",
+  },
+];
+
+myCoding.forEach((curElem) => {
+  console.log(curElem.languageName);
+});
